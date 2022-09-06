6bb13161e9465c3ae0b92a835d1934c086a6f68f
Gabriel-biel <gabriel97gla98@gmail.com>
2022-09-06 00:26:54 -0400

    Funcionamento dos Botões

--- React/chapter-II/gmoney/src/components/NewTransactionModal/index.tsx
+++ React/chapter-II/gmoney/src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,17 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import incomeImg from '../../assets/Entradas.svg'
+import outcomeImg from '../../assets/Saidas.svg'
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import closeImg from '../../assets/Close.svg'
+import { useState } from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal 
       isOpen={isOpen} 
@@ -25,12 +30,35 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
         <h2>Cadastrar Transação</h2>
         <input 
           type="number"
-          placeholder="Título" />
+          placeholder="Título" 
+        />
         <input 
           type="number"
-          placeholder="Valor" />
+          placeholder="Valor"
+        />
+
+        <TransactionTypeContainer>
+          <RadioBox 
+            type='button'
+            onClick={() => {setType('deposit')}}
+            isActive={type === 'deposit'}
+          >
+            <img src={incomeImg} alt="incomeImg" />
+            <span>Entradas</span>
+          </RadioBox>
+          <RadioBox 
+            type='button'
+            onClick={() => {setType('withdraw')}}
+            isActive={type === 'withdraw'}
+          >
+            <img src={outcomeImg} alt="outcomeImg" />
+            <span>Saídas</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
         <input 
-          placeholder="Categoria" />
+          placeholder="Categoria" 
+        />
           <button type="submit">Cadastrar</button>
       </Container>
   </Modal>
