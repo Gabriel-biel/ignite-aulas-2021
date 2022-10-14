05af5aa2f0e1c59a3a5c9d59883ae5e63d035bff
Gabriel-biel <gabriel97gla98@gmail.com>
2022-10-14 00:08:28 -0400

    :hammer: New Chappter Init Study

---
+++ React/ignews/src/pages/index.tsx
@@ -0,0 +1,13 @@
+
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Iníncio | IG News</title>
+      </Head>
+      <h1>Hello <span>Word!</span></h1>
+    </>
+  )
+}
