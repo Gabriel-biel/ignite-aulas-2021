b09fc9d7a238200ce9095c7b7afa77c3f7012d0d
Gabriel-biel <gabriel97gla98@gmail.com>
2022-10-14 15:36:42 -0400

    :hammer: add page Home in igNews

--- React/ignews/src/pages/index.tsx
+++ React/ignews/src/pages/index.tsx
@@ -1,13 +1,26 @@
 
 import Head from 'next/head'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Iníncio | IG News</title>
+        <title>Home | IG News</title>
       </Head>
-      <h1>Hello <span>Word!</span></h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, Welcome</span>
+          <h1>News About the <span>React</span> World.</h1>
+          <p>
+            Get acess to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <picture>
+          <img src="/images/avatar.svg" alt="girlCoding" />
+        </picture>
+      </main>
     </>
   )
 }
