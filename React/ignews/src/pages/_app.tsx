ae16a76b4ad4cc49d0cfbc0fe9a3a2fd05af661d
Gabriel-biel <gabriel97gla98@gmail.com>
2022-10-16 22:57:54 -0400

    :hammer: fix bug Typescript

--- React/ignews/src/pages/_app.tsx
+++ React/ignews/src/pages/_app.tsx
@@ -1,14 +1,29 @@
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+
+import { SessionProvider } from 'next-auth/react'
+
 import '../styles/global.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
+interface MyAppProps extends AppProps{
+  pageProps: {
+    session: {
+      user: {
+        name: string;
+        id: string;
+      },
+      expires: any;
+    }
+  }
+}
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: MyAppProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
-  )
+    </SessionProvider>
+  ) 
 }
 
 export default MyApp
