a8d8d1c60bfb4a48817cbee8b1303d591330df89
Gabriel-biel <gabriel97gla98@gmail.com>
2022-10-17 16:35:40 -0400

    :heavy_check_mark: Usando FaunaDB

--- React/ignews/src/pages/api/auth/[...nextauth].ts
+++ React/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth"
+
 import GithubProvider from "next-auth/providers/github"
 
+import { fauna } from '../../../services/fauna'
+import { query as q } from 'faunadb'
+
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -13,7 +16,39 @@ export const authOptions = {
         },
       }
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      }
+      catch {
+        return false
+      }
+    },
+  }
 }
 export default NextAuth(authOptions) 
